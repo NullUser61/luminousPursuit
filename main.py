b9ad2be087a75f1f080b9e54f8a387b115b7a229
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2024-05-02 11:10:53 +0530

    Initial UI Setup

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ from src.faceDetection.faceDetection import camera
 from src.faceDetection.centerface import CenterFace
 from src.faceDetection.faceDetection import recognize_faces
 import time
+from src.UI.main_ui import runUI
 # Initialize the serial port for communication with the laser module
 # serialInst = serial.Serial("/dev/ttyUSB0", baudrate=9600)
 
 
 def initialize():
-    def initialize():
-        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-        os.environ['CUDA_VISIBLE_DEVICES'] = '0'  # Specify the GPU device index
-        # Explicitly allow memory growth, which can help to reduce memory fragmentation
-        gpus = tf.config.experimental.list_physical_devices('GPU')
-        if gpus:
-            try:
-                for gpu in gpus:
-                    tf.config.experimental.set_memory_growth(gpu, True)
-            except RuntimeError as e:
-                print(e)
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'  # Specify the GPU device index
+    # Explicitly allow memory growth, which can help to reduce memory fragmentation
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+        except RuntimeError as e:
+            print(e)
 
 def load_models():
     global  facenet, model, encoder, haarcascade , centerface
@@ -46,11 +46,11 @@ def load_models():
     # haarcascade = cv.CascadeClassifier("../../data/haarcascade_frontalface_default.xml")
 
 def main():
-    initialize()
-    load_models()
-    camera( facenet, model, encoder, centerface, Recognise=1)
-    recognize_faces()
-
+    # initialize()
+    # load_models()
+    # camera( facenet, model, encoder, centerface, Recognise=1)
+    # recognize_faces()
+    runUI()
 
     cv.destroyAllWindows()
 
---
+++ src/UI/OpenCV.py
@@ -0,0 +1,59 @@
+import os
+import cv2 as cv
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+# os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
+
+# img = cv.imread('images/IMG_20230827_130752156.jpg')
+
+# cv.imshow('Cat', img)
+
+def rescaleFrame(frame, scale):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+
+    dimensions = (width, height)
+    
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+# capture = cv.VideoCapture('videos/VID_20230820_181931749.mp4')
+
+class Worker(QThread):
+    ImageUpdate = pyqtSignal(QImage)
+    def run(self):
+        self.ThreadActive = True
+        capture = cv.VideoCapture(0)
+        if (capture.isOpened() == False):
+            print("Error opening the file")
+        else:
+            # i = 0
+
+            while self.ThreadActive:
+                isTrue, frame = capture.read()
+
+                if (not isTrue):
+                    print("Error")
+                else:
+                    image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+                    flippedImage = cv.flip(image, 1)
+                    QtFormattedImage = QImage(flippedImage.data, flippedImage.shape[1], flippedImage.shape[0], QImage.Format_RGB888) 
+                    pic = QtFormattedImage.scaled(640, 480, Qt.KeepAspectRatio)
+
+                    self.ImageUpdate.emit(pic)
+                    # newFrame = rescaleFrame(frame, 0.75)
+                    # cv.imshow('Video', newFrame)
+                    # key = cv.waitKey(20)
+            
+                #     if key == ord('q'):
+                #         break
+
+                # i += 1
+
+            capture.release()
+        # cv.destroyAllWindows()
+    
+    def stop(self):
+        self.ThreadActive = False
+        self.quit()
+# cv.waitKey(0)
